d062e5cdcadacfdb78e54046abc63194321e36ae
akosifade25 <faderagao.aaron.dll@gmail.com>
2025-05-10 19:16:05 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer className="footer">
       <img
-        src="./assets/spyxfamilylogo.png"
+        src="/public/assets/spyxfamilylogo.png"
         alt="Spy x Family Logo"
         className="footer-logo"
       />
@@ -15,14 +15,14 @@ function Footer() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src="./assets/netflix.png" alt="Netflix" className="footer-icon" />
+          <img src=/public/assets/netflix.png" alt="Netflix" className="footer-icon" />
         </a>
         <a
           href="https://www.crunchyroll.com/series/G4PH0WXVJ/spy-x-family?srsltid=AfmBOoroPRbhmEiFxTM_xvniyeDNxxRZcuwRGlRHe4ZhvMtxjHTU-MC6"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src="./assets/crunchyroll.png" alt="Crunchyroll" className="footer-icon" />
+          <img src="/public/assets/crunchyroll.png" alt="Crunchyroll" className="footer-icon" />
         </a>
       </div>
 
